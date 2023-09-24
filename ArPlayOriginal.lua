df5eeb0fa78f9087c9c0ffd8579c1c54de324192
Fux <109365408+Fuxfantx@users.noreply.github.com>
2023-09-25 02:42:04 +0800

    Sync

--- ArPlayOriginal.lua
+++ ArPlayOriginal.lua
@@ -1392,6 +1392,7 @@ function update(self, tslf)
 						if dt>SWEEP and (chint.w==0 or chint.w==-1) then
 							chint.w = 1
 							Ar__current_lost = Ar__current_lost + 1
+						elseif dt<-510 then break  -- Asserted to be Sorted.
 						end
 						--
 						local thisgo = false
@@ -1458,7 +1459,7 @@ function update(self, tslf)
 							set_position(vecs[i], thisgo)
 							send(thisgo, hash_ar_update, { (progress-chint.w)/spd, (chint.w-chint.z)/spd } )
 							ago_index = ago_index + 1
-						elseif dt>=-510 and dt<-370 then
+						elseif dt<-370 then
 							vecs[i].x = chint.x
 							vecs[i].y = chint.y
 							vecs[i].z = -0.75 - dt*0.00001
